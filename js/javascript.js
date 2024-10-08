4bd0256ef39c67e1939fcd14e150af46152a36bf
CraftedRNL <126Rliu@frhsd.com>
2024-10-08 14:02:04 -0400

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,44 @@
+// alert('To be, Or to be?');
+
+// console.log(1);
+
+// console.log(2);
+
+// let age = 25;
+// let year = 2019;
+
+// // log this to console
+// // console.log(age, year);
+
+// age = 30;
+// console.log(age);
+
+// const points = 100;
+// // points = 50;
+// console.log(points)
+
+// var score = 75;
+// console.log(score);
+
+
+// string
+console.log('hello, world');
+let email = '126Rliu@frhsd.com'
+console.log(email);
+// string concatenation
+let firstName = 'Ryan';
+let lastName = 'Liu';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+// getting characters
+console.log(fullName[2]);
+// string length
+console.log(fullName.length);
+// string methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@')
+console.log(index);
