714c8e949d1ced2b585fba10a54012029ab88fdd
CraftedRNL <126Rliu@frhsd.com>
2024-10-10 12:14:05 -0400

    "ARRAYS"

--- js/javascript.js
+++ js/javascript.js
@@ -22,23 +22,121 @@
 
 
 // string
-console.log('hello, world');
-let email = '126Rliu@frhsd.com'
-console.log(email);
-// string concatenation
-let firstName = 'Ryan';
-let lastName = 'Liu';
+// console.log('hello, world');
+// let email = '126Rliu@frhsd.com'
+// console.log(email);
+// // string concatenation
+// let firstName = 'Ryan';
+// let lastName = 'Liu';
 
-let fullName = firstName + ' ' + lastName;
-console.log(fullName);
-// getting characters
-console.log(fullName[2]);
-// string length
-console.log(fullName.length);
-// string methods
-console.log(fullName.toUpperCase());
-let result = fullName.toLowerCase();
-console.log(result, fullName);
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+// // getting characters
+// console.log(fullName[2]);
+// // string length
+// console.log(fullName.length);
+// // string methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
 
-let index = email.indexOf('@')
-console.log(index);
+// let index = email.indexOf('@')
+// console.log(index);
+
+// common string methods
+
+// let email = '126Rliu@frhsd.com';
+
+// // let result = email.lastIndexOf('f');
+
+// //let result = email.slice(4,10);
+
+// // let result = email.substring(4,10);
+
+// //let result = email.replace('@', '?');
+
+// let result = email.replace('R','W')
+
+// console.log(result);
+
+// let radius = 10;
+// const pi = 3.14;
+
+// //console.log(radius, pi);
+// //math operator +, -, *, /, **. %
+
+// //console.log(10/2);
+// //let result = radius % 3;
+// // let result = pi * radius **2;
+
+// //order of operation - B I D M A S
+
+// //let result = 5 * (10-3)**2;
+
+// // console.log(result);
+
+// let likes = 10;
+
+// // likes = likes + 1;
+// // likes++;
+// // likes--;
+
+// // likes += 10;
+// // likes -= 5;
+// // likes *= 2;
+// // likes /=2;
+
+// // console.log(likes);
+
+// //NaN - not a number
+
+// // console.log(5 / 'hello');
+// // console.log(5 * 'hello');
+
+// let result = 'the blog has' + likes + 'likes';
+// console.log(result);
+
+
+//template string
+// const title = 'Best reads of 2019';
+// const author = 'Mario';
+// const likes = 30;
+
+// // concatenation way
+// // let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes.';
+// // console.log(result);
+
+// // template string way
+// let result = `The blog called ${title} by ${author} has ${likes} likes.`;
+// console.log(result);
+
+// // creating html templates
+// let html = `
+// <h2>${title}</h2>
+// <p>By ${author}</p>
+// <span>This blog had ${likes} likes </span>
+// `;
+// console.log(html);
+
+
+// arrays
+// let ninjas = ['shaun', 'ryu', 'chun-li'];
+// // ninjas[1] = 'ken';
+// // console.log(ninjas[1]);
+
+// // let ages = [20, 25, 30, 35];
+// // console.log(ages[2]);
+
+// // let random = ['shaun', 'crystal', 30 ,20];
+// // console.log(random);
+
+// // console.log(ninjas.length);
+
+// // array methods
+
+// // let result = ninjas.join('-');
+// // let result = ninjas.indexOf('chun-li');
+// // let result = ninjas.concat(['ken', 'crystal']);
+// let result = ninjas.push('ken');
+//  result = ninjas.pop('');
+// console.log(result);
