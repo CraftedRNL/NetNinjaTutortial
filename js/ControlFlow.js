e5b8d1405380cc41a0a68dbb856ad45e9918d504
CraftedRNL <126Rliu@frhsd.com>
2024-10-11 13:41:33 -0400

    LOGICAL KNOT

---
+++ js/ControlFlow.js
@@ -0,0 +1,86 @@
+// for loops
+
+// for(let i = 0; i < 5; i++) {
+//     console.log('in loop:', i);
+// }
+// console.log('loop finished');
+
+// const names = ['shaun', 'mario', "luigi"];
+
+// for(let i = 0; i < names.length; i++){
+//     // console.log(i);
+//     // console.log(names[i]);
+//     let html = `<div>${names[i]}</div>`
+//     console.log(html);
+// }
+
+// // while loops
+// const names =['shaun', 'mario', 'luigi'];
+// // let i = 0
+
+// // while(i < 5) {
+// //     console.log('in loop:', i);
+// //     i++;
+// // }
+
+// let i = 0;
+// while(i < names.length) {
+//     console.log(names[i]);
+//     i++;
+// }
+
+
+// do while loops
+// let i = 0;
+// do{
+//     console.log('val of i is:', i);
+//     i++;
+// } while(i < 5);
+
+// if statements 
+
+// const age = 25;
+
+// if(age > 20){
+//     console.log('you are over 20 years old');
+// }
+
+// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
+
+// if(ninjas.length > 4){
+//     console.log("that's a lot of ninjas");
+// // }
+
+// const password = 'p@ss';
+
+// if(password.length >= 8) {
+//     console.log('that password is long enough!');
+// }
+
+// else if statements
+
+
+// const password = 'p@ss';
+
+
+
+// if(password.length >= 12){
+//     console.log('that password is migthy strong');
+// }else if(password.length >= 8) {
+//     console.log('that password is long enough!');
+// } else {
+//     console.log('password is not long enough');
+// }
+
+// logical operators - or || and AND &&
+
+// const password = 'p@ss';
+
+// if(password.length >= 12 && password.includes('@')){
+//     console.log('that password is migthy strong');
+// }else if(password.length >= 8 || password.includes('@') && password.length > 5) {
+//     console.log('that password is strong enough!');
+// } else {
+//     console.log('password is not strong enough');
+// }
+
--- js/javascript.js
+++ js/javascript.js
@@ -140,4 +140,70 @@
 // let result = ninjas.push('ken');
 //  result = ninjas.pop('');
 // console.log(result);
-//fix
+
+// let age = null;
+
+// console.log(age, age + 3, `the age is ${age}`);
+
+// booleans & comparisions
+// console.log(true, false, "true", "false");
+
+// // methods can return booleans
+// let email = '126Rliu@frhsd.com';
+// let names = ['mario', 'luigi', 'toad']
+
+// // // let result = email.includes("!");
+// // let result = names.includes('bowser')
+
+// // console.log(result);
+
+// // comparison operators
+// 
+// // console.log(age == 25);
+// // console.log(age == 30);
+// // console.log(age != 30);
+// // console.log(age != 30);
+// // console.log(age > 20);
+// // console.log(age < 20);
+// // console.log(age <= 25);
+// // // console.log(age >= 25);
+
+// // let name = 'shaun';
+
+// // console.log(name == 'shaun');
+// // console.log(name == 'Shaun');
+// // console.log(name > 'crystal');
+// // console.log(name > 'Shaun');
+// // console.log(name > 'Crystal');
+
+// let age = 25;
+
+// //loose comparison (different types can still be equal)
+// // console.log(age == 25);
+// // console.log(age == '25');
+// // console.log(age != 25);
+// // console.log(age != '25');
+
+// // strict comparison (different types cannot be equal)
+
+// console.log(age === 25);
+// console.log(age === "25");
+// console.log(age !== 25);
+// // console.log(age !== "25");
+
+// // type conversion
+// let score = '100';
+
+// // score = Number(score)
+// // console.log(score + 1);
+// // console.log(typeof score);
+
+// // let result = Number('hello');
+// // let result = String(50);
+// // let result = Boolean(0);
+// let result = Boolean('');
+// console.log(result, typeof result);
+
+
+
+
