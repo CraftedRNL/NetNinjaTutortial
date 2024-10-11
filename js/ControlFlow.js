f9f49b15aba238f980ab16b814df23813158b8f0
CraftedRNL <126Rliu@frhsd.com>
2024-10-11 13:44:22 -0400

    NOT LOGICAL KNOT, LOGICAL OPERATORS

--- js/ControlFlow.js
+++ js/ControlFlow.js
@@ -76,6 +76,7 @@
 
 const password = 'p@ss';
 
+
 if(password.length >= 12 && password.includes('@')){
     console.log('that password is migthy strong');
 }else if(password.length >= 8 || password.includes('@') && password.length > 5) {
