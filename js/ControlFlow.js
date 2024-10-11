3ddb8b1dbd4e1fbf8c5f8c1482bc944e382d9abd
CraftedRNL <126Rliu@frhsd.com>
2024-10-11 13:42:54 -0400

    LOGICAL KNOT2.0

--- js/ControlFlow.js
+++ js/ControlFlow.js
@@ -74,13 +74,13 @@
 
 // logical operators - or || and AND &&
 
-// const password = 'p@ss';
+const password = 'p@ss';
 
-// if(password.length >= 12 && password.includes('@')){
-//     console.log('that password is migthy strong');
-// }else if(password.length >= 8 || password.includes('@') && password.length > 5) {
-//     console.log('that password is strong enough!');
-// } else {
-//     console.log('password is not strong enough');
-// }
+if(password.length >= 12 && password.includes('@')){
+    console.log('that password is migthy strong');
+}else if(password.length >= 8 || password.includes('@') && password.length > 5) {
+    console.log('that password is strong enough!');
+} else {
+    console.log('password is not strong enough');
+}
 
